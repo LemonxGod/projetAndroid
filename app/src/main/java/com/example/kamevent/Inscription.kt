8bf80ac435472fea19b4a9be166c627a4ccfaa3d
Massmas <33931611+Massmas@users.noreply.github.com>
2022-02-21 00:57:42 +0100

    Adding inscription, Adding redirecting to inscription

---
+++ app/src/main/java/com/example/kamevent/Inscription.kt
@@ -0,0 +1,11 @@
+package com.example.kamevent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Inscription : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.inscription)
+    }
+}
--- app/src/main/java/com/example/kamevent/MainActivity.kt
+++ app/src/main/java/com/example/kamevent/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.kamevent
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener{
+
+    lateinit var buttonInscription : Button
+    lateinit var buttonConnexion  : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        this.buttonInscription = findViewById(R.id.button2) as Button
+        this.buttonInscription.setOnClickListener(this)
+        this.buttonConnexion = findViewById(R.id.button) as Button
+        this.buttonConnexion.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            /* R.id.buttonConnexion -> {
+                 val i = Intent(this@MainActivity, FilActualite::class.java)
+                 startActivity(i)
+             }*/
+            R.id.button2 -> {
+                val i = Intent(this, Inscription::class.java)
+                startActivity(i);
+            }
+        }
     }
 }
+
+annotation class override
