8a74804891928665986b2c577d7707b5ad5b3d5d
LemonxGod <helloben60@gmail.com>
2022-02-27 16:57:02 +0100

    Ajout du logo sur la page de connexion et inscription, plus bouton retour inscription

--- app/src/main/java/com/example/kamevent/Inscription.kt
+++ app/src/main/java/com/example/kamevent/Inscription.kt
@@ -1,11 +1,35 @@
 package com.example.kamevent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
 
-class Inscription : AppCompatActivity() {
+
+
+class Inscription : AppCompatActivity() ,View.OnClickListener{
+    lateinit var returnBouton  : ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.inscription)
+        this.returnBouton = findViewById(R.id.returnButton) as ImageButton
+        this.returnBouton.setOnClickListener(this)
+    }
+
+    fun returnFunction(view: View) {
+        when (view.id) {
+            R.id.returnButton -> {
+                val i = Intent(this, MainActivity::class.java)
+                startActivity(i);
+            }
+        }
+    }
+
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
     }
 }
+
