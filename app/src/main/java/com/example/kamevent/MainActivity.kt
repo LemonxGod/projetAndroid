763c722b5d388fc7917dd6b8f7746280c4e8a71b
Rettana <rettana.muon@gmail.com>
2022-02-27 17:08:15 +0100

    Add SignIn Functions

--- app/src/main/java/com/example/kamevent/MainActivity.kt
+++ app/src/main/java/com/example/kamevent/MainActivity.kt
@@ -1,17 +1,26 @@
 package com.example.kamevent
 
 import android.content.Intent
+import android.nfc.Tag
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.example.kamevent.auth.SignInConnection
 
 class MainActivity : AppCompatActivity(), View.OnClickListener{
 
     lateinit var buttonInscription : Button
     lateinit var buttonConnexion  : Button
+    var connection : SignInConnection =  SignInConnection()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("MainActivity", "Creating Main Activity...")
+        connection.signInWithPassword("a@a.fr", "aaaaaa");
+        connection.signInWithPassword("a@a.f", "aaaaaa");
+        connection.signInWithPassword("a@a.fr", "aaaaa");
+        connection.signInWithPassword("v@a.fr", "aaaa");
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         this.buttonInscription = findViewById(R.id.button2) as Button
---
+++ app/src/main/java/com/example/kamevent/auth/FirebaseAuth.kt
@@ -0,0 +1,86 @@
+package com.example.kamevent.auth
+
+import android.app.Activity
+import android.content.ContentValues.TAG
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+public class UserActivity : Activity() {
+    private lateinit var auth: FirebaseAuth
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth = Firebase.auth
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        var currentUser = auth.currentUser
+        if (currentUser != null){
+            reload();
+        }
+    }
+
+    private fun createAccount(email: String, password: String) {
+        // [START create_user_with_email]
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+        // [END create_user_with_email]
+    }
+
+    private fun signIn(email: String, password: String) {
+        // [START sign_in_with_email]
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+        // [END sign_in_with_email]
+    }
+
+    private fun sendEmailVerification() {
+        // [START send_email_verification]
+        val user = auth.currentUser!!
+        user.sendEmailVerification()
+            .addOnCompleteListener(this) { task ->
+                // Email Verification sent
+            }
+        // [END send_email_verification]
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+
+    }
+
+    private fun reload() {
+
+    }
+}
